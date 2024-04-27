e3c59b9bfcbc22aea1e5a64ec067277b27ab7fa9
Katya <katyasenior@gmail.com>
2024-04-27 22:21:14 +0100

    fixed error message bug and had the basic upgrade increase in cost with each purchase

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ export default function App() {
   const [cookieCount, setCookies] = useState(0);
   const [cps, setCps] = useState(0);
   const [isVisible, setIsVisible] = useState(false);
+  const [upgradeCount, setUpgradeCount] = useState(0);
+  const [upgradeCost, setUpgradeCost] = useState(10);
 
   //set cps
   useEffect(() => {
@@ -32,14 +34,17 @@ export default function App() {
 
   //buy an upgrade
   function buyUpgrade() {
-    if (cookieCount > 9) {
+    if (cookieCount >= upgradeCost) {
       setCps(cps + 1);
-      setCookies(cookieCount - 10);
+      setCookies(cookieCount - upgradeCost);
+      setUpgradeCost(upgradeCost + 1);
+      setUpgradeCount(upgradeCount + 1);
+    } else {
+      setIsVisible(true);
+      setTimeout(() => {
+        setIsVisible(false);
+      }, 3000);
     }
-    setIsVisible(true);
-    setTimeout(() => {
-      setIsVisible(false);
-    }, 5000);
   }
 
   //Buttons/html structure
@@ -56,6 +61,7 @@ export default function App() {
         <p>{cookieCount} cookies</p>
         {cps} cookies per second
         <p>How about an upgrade?</p>
+        <p>{upgradeCount} upgrades purchased so far</p>
         {isVisible && <ErrorMessage />}
       </div>
     </>
